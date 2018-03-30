cec879f511050cdc1cd390046497b24dd3e2929f
Israr786 <israrul786@gmail.com>
2018-03-30 01:54:25 -0700

    Initial Commit

---
+++ ArrayController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArrayController
+//
+//  Created by Apple on 3/30/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
